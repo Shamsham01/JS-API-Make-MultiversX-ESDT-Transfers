5ec7c34571d0b38a6349050485a19293067e08cd
HodlTokenClub <98131892+Shamsham01@users.noreply.github.com>
2024-12-03 23:42:38 +0000

    Update index.js

--- index.js
+++ index.js
@@ -11,22 +11,22 @@ const app = express();
 // Constants
 const PORT = process.env.PORT || 10000;
 const API_PROVIDER_URL = process.env.API_PROVIDER || "https://api.multiversx.com";
-const APP_CLIENT_NAME = "MultiversX Transfers API for Make.com Custom Apps";
+const APP_CLIENT_NAME = "MultiversX Transfers API for Make.com";
 
 // Initialize MultiversX Provider
 const provider = new ProxyNetworkProvider(API_PROVIDER_URL, { clientName: APP_CLIENT_NAME });
 
-// Middleware to add the provider to request objects
+// Middleware setup
+app.use(cors());
+app.use(bodyParser.json());
+
+// Middleware to add provider to requests
 app.use((req, res, next) => {
     req.provider = provider;
     next();
 });
 
-// Middleware setup
-app.use(cors()); // Enable CORS for all routes
-app.use(bodyParser.json()); // Parse JSON-encoded request bodies
-
-// Health Check Endpoint
+// Health check endpoint
 app.get('/health', (req, res) => {
     res.json({
         status: "API is live",
@@ -42,7 +42,7 @@ app.use('/transfers', transfersRoutes);
 
 // Error Handling Middleware
 app.use((err, req, res, next) => {
-    console.error('Unhandled error:', err.stack);
+    console.error('Unhandled error:', err.message);
     res.status(500).json({ error: "An unexpected error occurred.", details: err.message });
 });
 
